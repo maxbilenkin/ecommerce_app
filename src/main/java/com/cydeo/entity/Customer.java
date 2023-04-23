84c79d6e0bfb9fadfb589dfb315b6adc1cb9c726
Maxim <maxbilenkin@gmail.com>
2023-04-23 16:46:01 -0400

    Initial commit

---
+++ src/main/java/com/cydeo/entity/Customer.java
@@ -0,0 +1,26 @@
+package com.cydeo.entity;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+public class Customer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String userName;
+
+
+}
