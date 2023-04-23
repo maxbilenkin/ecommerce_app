3b4a7a6198378ae6898cdca0de491662c4289fae
Maxim <maxbilenkin@gmail.com>
2023-04-23 17:24:29 -0400

    ecommerce app

---
+++ src/main/java/com/cydeo/entity/Category.java
@@ -0,0 +1,25 @@
+package com.cydeo.entity;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+
+}
---
+++ src/main/java/com/cydeo/entity/Product.java
@@ -0,0 +1,33 @@
+package com.cydeo.entity;
+
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+import java.util.List;
+
+@Entity
+@NoArgsConstructor
+@Getter
+@Setter
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private BigDecimal price;
+    private Integer quantity;
+    private Integer remainingQuantity;
+
+    @ManyToMany
+    @JoinTable(name = "product_category_rel",
+               joinColumns = @JoinColumn(name = "p_id"),
+               inverseJoinColumns = @JoinColumn(name = "c_id"))
+    private List<Category> categoryList;
+
+}
